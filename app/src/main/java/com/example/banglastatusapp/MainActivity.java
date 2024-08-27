f724eccea8abb697036dc26d6562151220d0f868
jishan1019 <imranislamjishan200@gmail.com>
2024-08-27 22:25:36 +0600

    dower navigation impliment

--- app/src/main/java/com/example/banglastatusapp/MainActivity.java
+++ app/src/main/java/com/example/banglastatusapp/MainActivity.java
@@ -1,14 +1,101 @@
 package com.example.banglastatusapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    MaterialToolbar materialToolbar;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        materialToolbar = findViewById(R.id.materialToolbar);
+        navigationView = findViewById(R.id.navigationView);
+
+
+        // ----------------------- Drawer Layout------------------------
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                MainActivity.this, drawerLayout, materialToolbar,R.string.nav_close, R.string.nav_open);
+        drawerLayout.addDrawerListener(toggle);
+
+
+        loadDrawerNavigation();
+        loadToolbarItem();
+
+
+
+
     }
+
+    //=========================== On Create end here ============================================
+
+
+    // -----------------------Drawer Navigation View------------------------
+    private void loadDrawerNavigation(){
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if (item.getItemId() == R.id.nav_home){
+
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }else if (item.getItemId() == R.id.favourite){
+
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }else if (item.getItemId() == R.id.rating){
+
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }else if (item.getItemId() == R.id.moreApps){
+
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }else if (item.getItemId() == R.id.privacyPolicy){
+
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }
+
+                return true;
+            }
+        });
+    }
+
+
+    //----------------------- Toolbar item --------------------------------
+    private void loadToolbarItem(){
+        materialToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                if (item.getItemId() == R.id.toolbarNotification){
+
+                }
+
+                return false;
+            }
+        });
+
+    }
+
+
+
+
+
 }
--- app/src/main/java/com/example/banglastatusapp/SplashActivity.java
+++ app/src/main/java/com/example/banglastatusapp/SplashActivity.java
@@ -1,18 +1,51 @@
 package com.example.banglastatusapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    Animation botom_anim , top_anim;
+    CardView logo1;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        getWindow().setStatusBarColor(getResources().getColor(R.color.white, getTheme()));
+
+        text =  findViewById(R.id.stext);
+        logo1 =  findViewById(R.id.logo1);
+        top_anim = AnimationUtils.loadAnimation(this, R.anim.top_anim);
+        botom_anim = AnimationUtils.loadAnimation(this, R.anim.botom_anim);
+        logo1.setAnimation(this.top_anim);
+        text.setAnimation(this.botom_anim);
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                SplashActivity.this.startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                SplashActivity.this.finish();
+            }
+        }, 2000);
+
+
+
+
+
+
     }
 
-    //
+    //=========================== On Create end here ============================================
 
 
 
